85f25147f04378998aa27f9293fcf2dfc9fbac0e
matheexe <matheus07bb@outlook.com>
2025-09-05 17:08:14 -0300

    Criando a sessão de planos.

---
+++ src/main.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const buttons = document.querySelectorAll('[data-tab-btn]');
+    const tabsContainer= document.querySelectorAll('[data-tab-id]')
+
+    for (let i = 0; i < buttons.length; i++){
+        buttons[i].addEventListener('click', function(btn){
+            const tabAdd = btn.target.dataset.tabBtn;
+            const tab = document.querySelector(`[data-tab-id=${tabAdd}]`);
+            hideAllTabs();
+            tab.classList.add('shows__list--active');
+            hideBtnActive();
+            btn.target.classList.add('shows__tabs__button--active');
+        })
+    }
+})
+
+function hideBtnActive(){
+    const buttons = document.querySelectorAll('[data-tab-btn]');
+
+    for (let i = 0; i < buttons.length; i++){
+        buttons[i].classList.remove('shows__tabs__button--active');
+    }
+
+}
+
+function hideAllTabs(){
+    const tabsContainer= document.querySelectorAll('[data-tab-id]')
+
+    for (let i = 0; i < tabsContainer.length; i++){
+        tabsContainer[i].classList.remove('shows__list--active');
+    }
+}
