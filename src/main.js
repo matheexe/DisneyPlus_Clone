d84f53aabe34dd2ef92345e9531090f2b4ba8573
matheexe <matheus07bb@outlook.com>
2026-02-23 19:45:33 -0300

    Alteração nos arquivos e adição do FAQ e Footer

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-btn]');
-    const tabsContainer= document.querySelectorAll('[data-tab-id]')
+    const tabsContainer= document.querySelectorAll('[data-tab-id]');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
     for (let i = 0; i < buttons.length; i++){
         buttons[i].addEventListener('click', function(btn){
@@ -12,7 +13,18 @@ document.addEventListener('DOMContentLoaded', function(){
             btn.target.classList.add('shows__tabs__button--active');
         })
     }
+
+    for (let i = 0; i < questions.length; i++){
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
 })
+
+function abreOuFechaResposta(e){
+    const classe ='faq__questions__item--is-open';
+    const elementoPai = e.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
 
 function hideBtnActive(){
     const buttons = document.querySelectorAll('[data-tab-btn]');
